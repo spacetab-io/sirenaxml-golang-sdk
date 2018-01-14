a0f9530e9a4b3dc542141badd2f2486657b91905
Eugene Toropov <eugene@skoosh.com>
2018-01-14 23:13:53 +0300

    Implemented <add_remark> Sirena method

---
+++ sirena/add.remark.response.types.go
@@ -0,0 +1,21 @@
+package sirena
+
+import "encoding/xml"
+
+// AddRemarkResponse is a Sirena response to <add_remark> request
+type AddRemarkResponse struct {
+	Answer  AddRemarkAnswer `xml:"answer"`
+	XMLName xml.Name        `xml:"sirena" json:"-"`
+}
+
+// AddRemarkAnswer is an <answer> section in Sirena <add_remark> response
+type AddRemarkAnswer struct {
+	Pult      string                   `xml:"pult,attr,omitempty"`
+	AddRemark AddRemarkAnswerAddRemark `xml:"add_remark"`
+}
+
+// AddRemarkAnswerAddRemark is a <add_remark> section in Sirena <add_remark> response
+type AddRemarkAnswerAddRemark struct {
+	Ok    *struct{} `xml:"ok,omitempty"`
+	Error *Error    `xml:"error,omitempty"`
+}
--- sirena/get.itin.receipts.response.types.go
+++ sirena/get.itin.receipts.response.types.go
@@ -10,12 +10,12 @@ type GetItinReceiptsResponse struct {
 
 // GetItinReceiptsAnswer is an <answer> section in Sirena <get_itin_receipts> response
 type GetItinReceiptsAnswer struct {
-	Answer          string                  `xml:"answer,attr,omitempty"`
-	GetItinReceipts GetItinReceiptsReceipts `xml:"get_itin_receipts>receipts"`
+	Answer          string                        `xml:"answer,attr,omitempty"`
+	GetItinReceipts GetItinReceiptsAnswerReceipts `xml:"get_itin_receipts>receipts"`
 }
 
-// GetItinReceiptsReceipts is a <receipts> element in Sirena <get_itin_receipts> response
-type GetItinReceiptsReceipts struct {
+// GetItinReceiptsAnswerReceipts is a <receipts> element in Sirena <get_itin_receipts> response
+type GetItinReceiptsAnswerReceipts struct {
 	CrTime string `xml:"cr_time,attr"` // "TimeDate" format
 	Value  string `xml:",chardata"`
 	Error  *Error `xml:"error,omitempty"`
