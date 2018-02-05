1ca1d5e63a2645ea57d4e033fce886e480fb9dcb
Eugene Toropov <eugene@skoosh.com>
2018-02-05 23:10:21 +0300

    Implemented <add_ff_info> request

---
+++ sirena/add.ff.info.response.types.go
@@ -0,0 +1,21 @@
+package sirena
+
+import "encoding/xml"
+
+// AddFFInfoResponse is a Sirena response to <add_ff_info> request
+type AddFFInfoResponse struct {
+	Answer  AddFFInfoAnswer `xml:"answer"`
+	XMLName xml.Name        `xml:"sirena" json:"-"`
+}
+
+// AddFFInfoAnswer is an <answer> section in Sirena <add_ff_info> response
+type AddFFInfoAnswer struct {
+	Pult      string                   `xml:"pult,attr,omitempty"`
+	AddFFInfo AddFFInfoAnswerAddFFInfo `xml:"add_ff_info"`
+}
+
+// AddFFInfoAnswerAddFFInfo is a <add_ff_info> section in Sirena <add_ff_info> response
+type AddFFInfoAnswerAddFFInfo struct {
+	Ok    *struct{} `xml:"ok,omitempty"`
+	Error *Error    `xml:"error,omitempty"`
+}
