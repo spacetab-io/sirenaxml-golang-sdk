44d100a2ffebfa313cffd6feed01b05fc35a6bd2
Eugene Toropov <eugene.toropov@gmail.com>
2019-04-11 17:04:45 +0300

    Changes in set_agent_discount request types

--- sirena/set.agent.discount.request.types.go
+++ sirena/set.agent.discount.request.types.go
@@ -33,9 +33,9 @@ type SetAgentDiscountUnit struct {
 
 // SetAgentDiscountFare is a fare element in set_agent_discount request
 type SetAgentDiscountFare struct {
-	Discount int    `xml:"discount,attr"`
-	Brand    string `xml:"brand,attr"`
-	Value    string `xml:",chardata"`
+	Discount float64 `xml:"discount,attr"`
+	Brand    string  `xml:"brand,attr"`
+	Value    string  `xml:",chardata"`
 }
 
 // SetAgentDiscountRequestParams is a <request_params> section in set_agent_discount request
