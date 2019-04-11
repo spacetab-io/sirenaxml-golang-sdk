45a2c1be55394c407f3b6aa4de01ec49285a0aa3
Eugene Toropov <eugene.toropov@gmail.com>
2019-04-11 16:14:30 +0300

    Changes in order request types

--- sirena/order.request.types.go
+++ sirena/order.request.types.go
@@ -31,18 +31,19 @@ type OrderRequestParams struct {
 
 // OrderAnswerParams is a <answer_params> section in <order> request
 type OrderAnswerParams struct {
-	Tickinfo           bool `xml:"tickinfo"`
-	ShowTickinfoAgency bool `xml:"show_tickinfo_agency"`
-	ShowActions        bool `xml:"show_actions"`
-	AddCommonStatus    bool `xml:"add_common_status"`
-	ShowUptRec         bool `xml:"show_upt_rec"`
-	AddRemarks         bool `xml:"add_remarks"`
-	AddSsr             bool `xml:"add_ssr"`
-	AddPaycode         bool `xml:"add_paycode"`
-	ShowErsp           bool `xml:"show_ersp"`
-	ShowInsuranceInfo  bool `xml:"show_insurance_info"`
-	ShowZh             bool `xml:"show_zh"`
-	AddRemoteRecloc    bool `xml:"add_remote_recloc"`
-	ShowComission      bool `xml:"show_comission"`
-	ShowBagNorm        bool `xml:"show_bag_norm"`
+	Tickinfo           bool   `xml:"tickinfo,omitempty"`
+	ShowTickinfoAgency bool   `xml:"show_tickinfo_agency,omitempty"`
+	ShowActions        bool   `xml:"show_actions,omitempty"`
+	AddCommonStatus    bool   `xml:"add_common_status,omitempty"`
+	ShowUptRec         bool   `xml:"show_upt_rec,omitempty"`
+	AddRemarks         bool   `xml:"add_remarks,omitempty"`
+	AddSsr             bool   `xml:"add_ssr,omitempty"`
+	AddPaycode         bool   `xml:"add_paycode,omitempty"`
+	ShowErsp           bool   `xml:"show_ersp,omitempty"`
+	ShowInsuranceInfo  bool   `xml:"show_insurance_info,omitempty"`
+	ShowZh             bool   `xml:"show_zh,omitempty"`
+	AddRemoteRecloc    bool   `xml:"add_remote_recloc,omitempty"`
+	ShowComission      bool   `xml:"show_comission,omitempty"`
+	ShowBagNorm        bool   `xml:"show_bag_norm,omitempty"`
+	Lang               string `xml:"lang,omitempty"`
 }
