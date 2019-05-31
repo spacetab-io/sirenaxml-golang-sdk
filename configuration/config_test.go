cdf30d76dfb9f3871141ca27843efc73481aed83
Sergey Gladkovskiy <smgladkovskiy@gmail.com>
2019-05-31 13:07:43 +0300

    return ip in config because of failing tests

--- configuration/config_test.go
+++ configuration/config_test.go
@@ -17,6 +17,7 @@ func TestConfig_PrepareKeys(t *testing.T) {
 		sc := Config{
 			ClientID:                 clientID,
 			Environment:              os.Getenv("ENV"),
+			Ip:                       os.Getenv("IP"),
 			MaxConnections:           requestHandlersNum,
 			ClientPublicKey:          os.Getenv("CLIENT_PUBLIC_KEY"),
 			ClientPrivateKey:         os.Getenv("CLIENT_PRIVATE_KEY"),
@@ -34,6 +35,7 @@ func TestConfig_PrepareKeys(t *testing.T) {
 		sc := Config{
 			ClientID:                 clientID,
 			Environment:              os.Getenv("ENV"),
+			Ip:                       os.Getenv("IP"),
 			MaxConnections:           requestHandlersNum,
 			ClientPrivateKey:         os.Getenv("CLIENT_PRIVATE_KEY"),
 			ServerPublicKey:          os.Getenv("SERVER_PUBLIC_KEY"),
@@ -50,6 +52,7 @@ func TestConfig_PrepareKeys(t *testing.T) {
 		sc := Config{
 			ClientID:                 clientID,
 			Environment:              os.Getenv("ENV"),
+			Ip:                       os.Getenv("IP"),
 			MaxConnections:           requestHandlersNum,
 			ClientPublicKey:          os.Getenv("CLIENT_PUBLIC_KEY"),
 			ServerPublicKey:          os.Getenv("SERVER_PUBLIC_KEY"),
@@ -66,6 +69,7 @@ func TestConfig_PrepareKeys(t *testing.T) {
 		sc := Config{
 			ClientID:                 clientID,
 			Environment:              os.Getenv("ENV"),
+			Ip:                       os.Getenv("IP"),
 			MaxConnections:           requestHandlersNum,
 			ClientPublicKey:          os.Getenv("CLIENT_PUBLIC_KEY"),
 			ClientPrivateKey:         os.Getenv("CLIENT_PRIVATE_KEY"),
@@ -80,24 +84,33 @@ func TestConfig_PrepareKeys(t *testing.T) {
 }
 
 func TestConfig_GetAddr(t *testing.T) {
-	expectMaps := map[string][]string{
-		EnvLearning:   {"193.104.87.251:34323", "194.84.25.50:34323"},
-		EnvTesting:    {"193.104.87.251:34322", "194.84.25.50:34322"},
-		EnvProduction: {"193.104.87.251:34321", "194.84.25.50:34321"},
+	ip := "193.104.87.251"
+	expectMaps := map[string]string{
+		EnvLearning:   ip + ":34323",
+		EnvTesting:    ip + ":34322",
+		EnvProduction: ip + ":34321",
 	}
-	for env, slice := range expectMaps {
+	for env, ipAddress := range expectMaps {
 		t.Run("success "+env, func(t *testing.T) {
-			sc := Config{Environment: env}
+			sc := Config{Environment: env, Ip: ip}
 			addr, err := sc.GetAddr()
 			if !assert.NoError(t, err) {
 				t.FailNow()
 			}
-			assert.Subset(t, slice, []string{addr})
+			assert.Equal(t, ipAddress, addr)
 		})
 	}
 
-	t.Run("error", func(t *testing.T) {
-		sc := Config{}
+	t.Run("error no ip", func(t *testing.T) {
+		sc := Config{Environment: EnvProduction}
+		_, err := sc.GetAddr()
+		if !assert.Error(t, err) {
+			t.FailNow()
+		}
+	})
+
+	t.Run("error no env", func(t *testing.T) {
+		sc := Config{Ip: ip}
 		_, err := sc.GetAddr()
 		if !assert.Error(t, err) {
 			t.FailNow()
