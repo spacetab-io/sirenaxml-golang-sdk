e195c6de7b8ad569730484ed664c92d1f78f3784
Sergey Gladkovskiy <smgladkovskiy@gmail.com>
2019-06-02 10:24:13 +0300

    some more tests, corrections and makefile

--- crypt/crypt_test.go
+++ crypt/crypt_test.go
@@ -1,6 +1,7 @@
 package crypt
 
 import (
+	"io/ioutil"
 	"os"
 	"strings"
 	"testing"
@@ -8,11 +9,50 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	privateKey,
+	publicKey,
+	privateKeySecret,
+	publicKeySecret []byte
+	pass = "secret"
+	key  = []byte("deskey")
+)
+
+func tearUp() {
+	var err error
+	privateKey, err = ioutil.ReadFile("private.key.pem")
+	if err != nil {
+		panic(err)
+	}
+	publicKey, err = ioutil.ReadFile("public.key.pem")
+	if err != nil {
+		panic(err)
+	}
+
+	privateKeySecret, err = ioutil.ReadFile("private.secret.key.pem")
+	if err != nil {
+		panic(err)
+	}
+	publicKeySecret, err = ioutil.ReadFile("public.secret.key.pem")
+	if err != nil {
+		panic(err)
+	}
+	publicKeySecret, err = ioutil.ReadFile("public.secret.key.pem")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestMain(m *testing.M) {
+	tearUp()
+	retCode := m.Run()
+	os.Exit(retCode)
+}
+
 func TestEncryptDataWithServerPubKey(t *testing.T) {
 	data := []byte(RandString(8))
-	key := []byte(strings.ReplaceAll(os.Getenv("CLIENT_PUBLIC_KEY"), "\\n", "\n"))
 	t.Run("success", func(t *testing.T) {
-		encKey, err := EncryptDataWithServerPubKey(data, key)
+		encKey, err := EncryptDataWithServerPubKey(data, publicKey)
 		if !assert.NoError(t, err) {
 			t.FailNow()
 		}
@@ -32,4 +72,51 @@ func TestEncryptDataWithServerPubKey(t *testing.T) {
 			t.FailNow()
 		}
 	})
+}
+
+func TestDecryptDataWithClientPrivateKey(t *testing.T) {
+	data := []byte(RandString(8))
+	//serverPubKey := []byte(strings.ReplaceAll(os.Getenv("CLIENT_PUBLIC_KEY"), "\\n", "\n"))
+	//clientPrivateKey := []byte(strings.ReplaceAll(os.Getenv("CLIENT_PRIVATE_KEY"), "\\n", "\n"))
+	//pass := os.Getenv("CLIENT_PRIVATE_KEY_PASSWORD")
+	t.Run("success no password", func(t *testing.T) {
+		encData, err := EncryptDataWithServerPubKey(data, publicKey)
+		if err != nil {
+			t.FailNow()
+		}
+		decData, err := DecryptDataWithClientPrivateKey(encData, privateKey, "")
+		if !assert.NoError(t, err) {
+			t.FailNow()
+		}
+		assert.Equal(t, data, decData)
+	})
+	t.Run("success with password", func(t *testing.T) {
+		encData, err := EncryptDataWithServerPubKey(data, publicKeySecret)
+		if err != nil {
+			t.FailNow()
+		}
+		decData, err := DecryptDataWithClientPrivateKey(encData, privateKeySecret, pass)
+		if !assert.NoError(t, err) {
+			t.FailNow()
+		}
+		assert.Equal(t, data, decData)
+	})
+	t.Run("error with password", func(t *testing.T) {
+		encData, _ := EncryptDataWithServerPubKey(data, publicKey)
+		_, err := DecryptDataWithClientPrivateKey(encData, privateKey, "wrongSecret")
+		if !assert.Error(t, err) {
+			t.FailNow()
+		}
+	})
+}
+
+func TestGeneratePrivateKeySignature(t *testing.T) {
+	deskeyCrypted, err := EncryptDataWithServerPubKey(key, publicKey)
+	if !assert.NoError(t, err) {
+		t.FailNow()
+	}
+	_, err = GeneratePrivateKeySignature(deskeyCrypted, privateKey, "")
+	if !assert.NoError(t, err) {
+		t.FailNow()
+	}
 }
