9a280f5d6904984bf3826db2e98d6791c5c41763
Sergey Gladkovskiy <smgladkovskiy@gmail.com>
2019-06-05 11:46:12 +0300

    logger.XXXf() method correct attributes

--- logs/logger.go
+++ logs/logger.go
@@ -4,13 +4,13 @@ var Logger LogWriter
 
 type LogWriter interface {
 	Debug(args ...interface{})
-	Debugf(args ...interface{})
+	Debugf(format string, args ...interface{})
 	Info(args ...interface{})
-	Infof(args ...interface{})
+	Infof(format string, args ...interface{})
 	Warning(args ...interface{})
-	Warningf(args ...interface{})
+	Warningf(format string, args ...interface{})
 	Error(args ...interface{})
-	Errorf(args ...interface{})
+	Errorf(format string, args ...interface{})
 	Fatal(args ...interface{})
-	Fatalf(args ...interface{})
+	Fatalf(format string, args ...interface{})
 }
